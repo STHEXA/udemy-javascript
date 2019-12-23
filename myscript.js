4268adfef0a74a5c82e37011c0184e3d73ceda43
STHEXA <ti.syu.s@gmail.com>
2019-12-23 14:20:14 +0900

    up to switch

--- myscript.js
+++ myscript.js
@@ -1,1 +1,120 @@
-console.log("Hello World! from myscript.js");
+// console.log("Hello World! from my
+// メッセージを出力
+// console.log("メッセージを出力");
+
+// 変数を出力
+// var i = 10;
+// console.log(i);
+
+// info,warn,error
+// console.info("情報です");
+
+// console.warn("警告です");
+
+// console.error("エラーです");
+
+// var colorName = "red";
+// console.log(colorName);
+
+// const TAX_RATE = 1.08;
+// console.log(TAX_RATE);
+
+// console.log(10);
+// console.log("She's happy.");
+
+// var x = null;
+// console.log(x);
+
+// var y;
+// console.log(y);
+
+// 後置演算、前置演算
+// var x = 10;
+// var y = x++;
+// console.log(x);
+// console.log(y);
+
+// var x = 10;
+// var y = ++x;
+// console.log(x);
+// console.log(y);
+
+// x = 3;
+// console.log(x);
+// x += 3;
+// console.log(x);
+// x -= 3;
+// console.log(x);
+// x *= 3;
+// console.log(x);
+// x /= 3;
+// console.log(x);
+// x %= 3;
+// console.log(x);
+
+// x = 100 * 13;
+// console.log(x);
+
+// function関数
+// function getRectangle(height,width) {
+//   return height * width;
+// }
+
+// console.log(getRectangle(3, 5));
+
+// 関数リテラル
+// var getRectangle = function(height, width) {
+//   return height * width;
+// }
+
+// console.log(getRectangle(3, 5));
+
+// functionコンストラクター
+// var getRectangle = new Function('height', 'width', 'return height * width');
+// console.log(getRectangle(3, 5));
+
+// 演習
+// function calc(price, number){
+//   return price * number;
+// };
+
+// console.log(calc(100, 13));
+
+// 条件分岐
+// var x = 100;
+// var x = 99;
+// if(x >= 100){
+//   console.log('100以上です。');
+// }else {
+//   console.log('100未満です。');
+// }
+
+// var x = 59;
+
+// if(x >= 90){
+//   console.log('A');
+// }else if(x >= 80){
+//   console.log('B');
+// }else if(x >= 60){
+//   console.log('C');
+// }else{
+//   console.log('D');
+// }
+
+// switch
+var n = 4;
+
+switch (n) {
+  case 1:
+    console.log('数値は1です。');
+    break;
+  case 2:
+    console.log('数値は2です。');
+    break;
+  case 3:
+    console.log('数値は3です。');
+    break;
+  default:
+    console.log('範囲外です。');
+    break;
+}
