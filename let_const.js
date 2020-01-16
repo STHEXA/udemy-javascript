8b4fff7a3dbd04f18f32f4562311f859123a79ee
STHEXA <ti.syu.s@gmail.com>
2020-01-16 17:43:16 +0900

    ES2015

---
+++ let_const.js
@@ -0,0 +1,14 @@
+// まずvar
+// var i = 10;
+// i = 11;
+// var i = 12;
+
+//let
+// let j = 10;
+// j = 10;
+// let j = 12; //再宣言  NG
+
+//const
+const tax_rate = 0.08;
+tax_rate = 0.1; //再代入 NG
+const tax_rate = 0.2; //再宣言  NG
